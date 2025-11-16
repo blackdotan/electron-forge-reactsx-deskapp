7c8deec9f5e68013bd4cfbc6aae55a3a5ddedd53
black.an <wmw1612@gmail.com>
2025-11-16 20:11:09 +0800

    feat: upgrade react@18+

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
-import ReactDOM from "react-dom";
-import { App } from "./App";
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById('root');
+if (!container) {
+  throw new Error('Root element not found');
+}
+
+const root = createRoot(container);
+root.render(<App />);
